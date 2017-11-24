75c47403da6af55797c07ebcb637482b39d5adab
Cobin Bluth <cbluth@gmail.com>
2017-11-24 03:48:18 -0600

    add suid

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ls -lsha /home/jenkins/.jenkins
-su - root -c "/bin/sh /usr/local/bin/dockerd-entrypoint.sh &"
+/usr/bin/su - root -c "/bin/sh /usr/local/bin/dockerd-entrypoint.sh &"
 sleep 5
 /usr/bin/sudo chmod a+rw /var/run/docker.sock
 docker build -f ./Dockerfile ./
